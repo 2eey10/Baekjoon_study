489757199a2d9a4604d6355ea099e258b3b72366
2eey10 <coherentman0103@naver.com>
2024-01-22 22:19:03 +0900

    add 10807.py file

---
+++ Step-by-step explanation/1차원배열/10807.py
@@ -0,0 +1,12 @@
+import sys
+
+t = int(input())
+arr = list(map(int, sys.stdin.readline().split()))
+v = int(input())
+
+cnt = 0
+for i in range(t):
+    if arr[i] == v:
+        cnt += 1
+
+print(cnt)
